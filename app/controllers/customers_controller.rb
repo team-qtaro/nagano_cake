56f3aa90f114f6d96cdcd6a3c8d7ba0f2484f052
Masayoshi0609 <masayoshi9876@gmail.com>
2021-08-20 00:22:12 +0000

    [Start]customer

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,19 @@
+class CustomersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def confirm
+  end
+
+  def quit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+ resources :customers, except: [:new, :create, :destory]
+  get 'customers/confirm' => "customers#confirm"
+  get 'customers/quit'    => "customers#quit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
