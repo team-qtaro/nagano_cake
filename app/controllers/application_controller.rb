4c5319f8feab61ddb3562e4ae46681c658921119
mtnn <mtk8engr@gmail.com>
2021-08-21 09:52:12 +0000

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when Customer
+      root_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
 
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
   namespace :admin do
   end
 
-  namespace :public do
-    devise_for :customers
+  scope module: :public do
     root to: 'homes#top'
     get "about" => "homes#about" , as: "about"
   end
-end
 
+end
