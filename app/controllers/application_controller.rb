43e11847ee032966556f9980c25f650c04800489
mtnn <mtk8engr@gmail.com>
2021-08-22 05:29:01 +0000

    edit devise form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
 
   def after_sign_in_path_for(resource)
     case resource
@@ -12,6 +15,10 @@ class ApplicationController < ActionController::Base
     when Customer
       root_path
     end
+  end
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :first_name_kana, :last_name_kana, :telephone_number, :postal_code, :address])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :admins, controllers: {
-    sessions: 'admins/sessions',
-    passwords: 'admins/passwords',
-    registrations: 'admins/registrations'
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
   }
 
   devise_for :customers, controllers: {
