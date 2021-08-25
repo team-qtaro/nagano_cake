e264e7862762ac88956db55a8aed3c65f251b3b6
mtnn <mtk8engr@gmail.com>
2021-08-25 07:55:01 +0000

    fix form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
 
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+
+  devise_for :customers, controllers: {
+    sessions: 'public/sessions',
+    passwords: 'public/passwords',
+    registrations: 'public/registrations'
+  }
+
   scope module: :public do
     root to: 'homes#top'
     get "about" => "homes#about", as: "about"
-    
+
     resources :customers, only: [:show, :edit, :update]
     patch 'customers/quit'
-    
+
     resources :orders,only: [:index,:show,:new] do
       collection do
         post 'confirm'
@@ -19,22 +31,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :genres, only: [:index,:new,:create,:edit,:update]
     resources :orders, only: [:index,:show]
-  end
-
-  namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
-  
-  devise_for :admins, controllers: {
-    sessions: 'admin/sessions',
-    passwords: 'admin/passwords',
-    registrations: 'admin/registrations'
-  }
 
-  devise_for :customers, controllers: {
-    sessions: 'public/sessions',
-    passwords: 'public/passwords',
-    registrations: 'public/registrations'
-  }
-  
 end
