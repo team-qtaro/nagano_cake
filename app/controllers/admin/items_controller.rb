d14a0a5abb4bde598f431d045d7dbec3534cd0a1
Masayoshi0609 <masayoshi9876@gmail.com>
2021-08-27 04:23:41 +0000

    [Start]public_customer_quit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -25,9 +25,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to admin_item_path(item)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+     redirect_to admin_item_path(@item)
+    else
+     render :edit
+    end
   end
   
   private
